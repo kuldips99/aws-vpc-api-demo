0e86ab61bc66102b3430cee6306ee6747f190bc9
Kuldip <kuldipsingh_08@rediff.com>
2025-07-21 00:18:04 +0530

    Fixing USER_PASSWORD_AUTH issue

--- aws_vpc_api_demo/vpc_api_stack.py
+++ aws_vpc_api_demo/vpc_api_stack.py
@@ -2,7 +2,7 @@ from aws_cdk import (
     Stack,
     Duration,
     RemovalPolicy,
-    CfnOutput,                       
+    CfnOutput,
     aws_cognito as cognito,
     aws_dynamodb as ddb,
     aws_lambda as _lambda,
@@ -11,6 +11,7 @@ from aws_cdk import (
     aws_apigatewayv2_integrations as apigw_int,
     aws_apigatewayv2_authorizers as apigw_auth,
 )
+from aws_cdk.aws_cognito import AuthFlow
 from constructs import Construct
 import pathlib
 
@@ -20,23 +21,25 @@ class VpcApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1 Cognito
+        # 1 Cognito User Pool + App Client (enable USER_PASSWORD_AUTH)
         user_pool = cognito.UserPool(
             self, "Users",
             self_sign_up_enabled=True,
             sign_in_aliases=cognito.SignInAliases(email=True),
             password_policy=cognito.PasswordPolicy(min_length=8),
         )
-        user_pool_client = user_pool.add_client("spa-client")
+        user_pool_client = user_pool.add_client("spa-client",
+            auth_flows=AuthFlow(user_password=True)   # ← enable USER_PASSWORD_AUTH
+        )
 
-        # 2 DynamoDB
+        # 2 DynamoDB table
         table = ddb.Table(
             self, "VpcMetadata",
             partition_key=ddb.Attribute(name="vpc_id", type=ddb.AttributeType.STRING),
             removal_policy=RemovalPolicy.DESTROY,
         )
 
-        # 3 IAM role
+        # 3 IAM Role for Lambdas
         lambda_role = iam.Role(
             self, "LambdaExecRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
@@ -57,11 +60,12 @@ class VpcApiStack(Stack):
             },
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name(
-                    "service-role/AWSLambdaBasicExecutionRole")
+                    "service-role/AWSLambdaBasicExecutionRole"
+                )
             ],
         )
 
-        # 4 Lambda code
+        # 4 Lambda functions
         src_dir = str(pathlib.Path(__file__).parent / "lambda_src")
         common = dict(
             runtime=_lambda.Runtime.PYTHON_3_12,
@@ -76,12 +80,13 @@ class VpcApiStack(Stack):
         get_fn    = _lambda.Function(self, "GetVpcsFn",
                                      handler="get_vpcs.handler",  **common)
 
-        # 5 HTTP API
+        # 5 HTTP API + Cognito Authorizer
         http_api = apigw.HttpApi(
             self, "VpcApi",
             cors_preflight=apigw.CorsPreflightOptions(
                 allow_methods=[apigw.CorsHttpMethod.ANY],
-                allow_origins=["*"]),
+                allow_origins=["*"]
+            ),
         )
         authorizer = apigw_auth.HttpUserPoolAuthorizer(
             "CognitoAuth",
@@ -91,14 +96,17 @@ class VpcApiStack(Stack):
         http_api.add_routes(
             path="/vpcs", methods=[apigw.HttpMethod.POST],
             integration=apigw_int.HttpLambdaIntegration("CreateInt", create_fn),
-            authorizer=authorizer)
+            authorizer=authorizer
+        )
         http_api.add_routes(
             path="/vpcs", methods=[apigw.HttpMethod.GET],
             integration=apigw_int.HttpLambdaIntegration("GetInt", get_fn),
-            authorizer=authorizer)
+            authorizer=authorizer
+        )
 
-        # 6 Outputs  ← NEW
-        CfnOutput(self, "HttpApiUrl",         value=http_api.url)
-        CfnOutput(self, "UserPoolId",         value=user_pool.user_pool_id)
-        CfnOutput(self, "UserPoolClientId",   value=user_pool_client.user_pool_client_id)
+        # 6 CloudFormation Outputs
+        CfnOutput(self, "HttpApiUrl",       value=http_api.url)
+        CfnOutput(self, "UserPoolId",       value=user_pool.user_pool_id)
+        CfnOutput(self, "UserPoolClientId", value=user_pool_client.user_pool_client_id)
+
 
