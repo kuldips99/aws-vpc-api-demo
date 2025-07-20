8c126f66a092d6861b347f8aebb10626df8456ee
Kuldip <kuldipsingh_08@rediff.com>
2025-07-20 22:15:00 +0530

    Deleting everything and restarting again

--- api_vpc/lambda_src/get_vpcs.py
+++
@@ -1,7 +0,0 @@
-import json, os, boto3
-TABLE = boto3.resource("dynamodb").Table(os.environ["TABLE_NAME"])
-
-def handler(event, context):
-    items = TABLE.scan(ProjectionExpression="vpc_id, #n, cidr, subnets, timestamp",
-                       ExpressionAttributeNames={"#n": "name"})["Items"]
-    return {"statusCode": 200, "body": json.dumps(items)}
