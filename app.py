b5accd43ed3cfda72009a59a8bcfe9c4c9da0828
Kuldip <kuldipsingh_08@rediff.com>
2025-07-20 20:19:08 +0530

    Serverless VPC API stack

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
-
-from aws_vpc_api_demo.aws_vpc_api_demo_stack import AwsVpcApiDemoStack
-
+from api_vpc.stacks.vpc_api_stack import VpcApiStack   
 
 app = cdk.App()
-AwsVpcApiDemoStack(app, "AwsVpcApiDemoStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
+VpcApiStack(app, "VpcApiStack", env=cdk.Environment(
+    account="242201265757",
+    region="us-east-1"          
+))
 app.synth()
