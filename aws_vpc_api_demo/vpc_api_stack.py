8c3469cfe55dd4b2dda5f131966683eb03cbf1fe
Kuldip <kuldipsingh_08@rediff.com>
2025-07-20 23:38:58 +0530

    CDK stack with proper output

--- aws_vpc_api_demo/vpc_api_stack.py
+++ aws_vpc_api_demo/vpc_api_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack,
     Duration,
     RemovalPolicy,
+    CfnOutput,                       
     aws_cognito as cognito,
     aws_dynamodb as ddb,
     aws_lambda as _lambda,
@@ -19,7 +20,7 @@ class VpcApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1 Cognito (auth)
+        # 1 Cognito
         user_pool = cognito.UserPool(
             self, "Users",
             self_sign_up_enabled=True,
@@ -32,95 +33,72 @@ class VpcApiStack(Stack):
         table = ddb.Table(
             self, "VpcMetadata",
             partition_key=ddb.Attribute(name="vpc_id", type=ddb.AttributeType.STRING),
-            removal_policy=RemovalPolicy.DESTROY,  # lab convenience
+            removal_policy=RemovalPolicy.DESTROY,
         )
 
-        # 3 IAM role for Lambda
+        # 3 IAM role
         lambda_role = iam.Role(
             self, "LambdaExecRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
             inline_policies={
-                "VpcAndDdb": iam.PolicyDocument(
-                    statements=[
-                        iam.PolicyStatement(
-                            actions=[
-                                "ec2:CreateVpc",
-                                "ec2:CreateSubnet",
-                                "ec2:DescribeVpcs",
-                                "ec2:DescribeSubnets",
-                                "ec2:CreateTags",
-                            ],
-                            resources=["*"],
-                        ),
-                        iam.PolicyStatement(
-                            actions=["dynamodb:PutItem", "dynamodb:Scan"],
-                            resources=[table.table_arn],
-                        ),
-                    ]
-                )
+                "VpcAndDdb": iam.PolicyDocument(statements=[
+                    iam.PolicyStatement(
+                        actions=[
+                            "ec2:CreateVpc", "ec2:CreateSubnet", "ec2:CreateTags",
+                            "ec2:DescribeVpcs", "ec2:DescribeSubnets"
+                        ],
+                        resources=["*"],
+                    ),
+                    iam.PolicyStatement(
+                        actions=["dynamodb:PutItem", "dynamodb:Scan"],
+                        resources=[table.table_arn],
+                    ),
+                ])
             },
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name(
-                    "service-role/AWSLambdaBasicExecutionRole"
-                )
+                    "service-role/AWSLambdaBasicExecutionRole")
             ],
         )
 
         # 4 Lambda code
         src_dir = str(pathlib.Path(__file__).parent / "lambda_src")
-        common_args = dict(
+        common = dict(
             runtime=_lambda.Runtime.PYTHON_3_12,
-            timeout=Duration.seconds(30),
             memory_size=256,
+            timeout=Duration.seconds(30),
             role=lambda_role,
             environment={"TABLE_NAME": table.table_name},
             code=_lambda.Code.from_asset(src_dir),
         )
+        create_fn = _lambda.Function(self, "CreateVpcFn",
+                                     handler="create_vpc.handler", **common)
+        get_fn    = _lambda.Function(self, "GetVpcsFn",
+                                     handler="get_vpcs.handler",  **common)
 
-        create_fn = _lambda.Function(
-            self, "CreateVpcFn", handler="create_vpc.handler", **common_args
-        )
-        get_fn = _lambda.Function(
-            self, "GetVpcsFn", handler="get_vpcs.handler", **common_args
-        )
-
-        # 5 HTTP API + Cognito authorizer  (***FIX HERE***)
+        # 5 HTTP API
         http_api = apigw.HttpApi(
-            self,
-            "VpcApi",
+            self, "VpcApi",
             cors_preflight=apigw.CorsPreflightOptions(
                 allow_methods=[apigw.CorsHttpMethod.ANY],
-                allow_origins=["*"],
-            ),
+                allow_origins=["*"]),
         )
-
         authorizer = apigw_auth.HttpUserPoolAuthorizer(
             "CognitoAuth",
-            user_pool,                # <- positional arg
+            user_pool,
             user_pool_clients=[user_pool_client],
         )
-
         http_api.add_routes(
-            path="/vpcs",
-            methods=[apigw.HttpMethod.POST],
-            integration=apigw_int.HttpLambdaIntegration(
-                "CreateIntegration", create_fn
-            ),
-            authorizer=authorizer,
-        )
-
+            path="/vpcs", methods=[apigw.HttpMethod.POST],
+            integration=apigw_int.HttpLambdaIntegration("CreateInt", create_fn),
+            authorizer=authorizer)
         http_api.add_routes(
-            path="/vpcs",
-            methods=[apigw.HttpMethod.GET],
-            integration=apigw_int.HttpLambdaIntegration(
-                "GetIntegration", get_fn
-            ),
-            authorizer=authorizer,
-        )
+            path="/vpcs", methods=[apigw.HttpMethod.GET],
+            integration=apigw_int.HttpLambdaIntegration("GetInt", get_fn),
+            authorizer=authorizer)
 
-        # 6 Outputs (handy in the console)
-        self.http_api_url = http_api.url
-        self.user_pool_id = user_pool.user_pool_id
-        self.user_pool_client_id = user_pool_client.user_pool_client_id
-
+        # 6 Outputs  ← NEW
+        CfnOutput(self, "HttpApiUrl",         value=http_api.url)
+        CfnOutput(self, "UserPoolId",         value=user_pool.user_pool_id)
+        CfnOutput(self, "UserPoolClientId",   value=user_pool_client.user_pool_client_id)
 
